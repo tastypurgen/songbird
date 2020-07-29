6c2f57d626452157de77dd348191350025f5fd8a
Andrei Abramchuk <stilegs@gmail.com>
2020-07-29 19:49:19 +0300

    feat: add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <h2>Header</h2>
+    <header>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid="sm">
+          <Navbar.Brand href="#">
+            SongBird
+        </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Score: 0
+          </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
