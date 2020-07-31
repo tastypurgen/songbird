15091fac4cf8ef00a0b70126040eacac3709ba4e
Andrei Abramchuk <stilegs@gmail.com>
2020-07-31 11:21:08 +0300

    sryle: apply styles

--- src/components/answer/answer.jsx
+++ src/components/answer/answer.jsx
@@ -5,8 +5,25 @@ const flagsPath = process.env.PUBLIC_URL + '/assets/flags/';
 export default function Answer({ flag, language }) {
   console.log(flag)
   return (
-    <div>
-      <img style={{ width: '30px', height: '30px', borderRadius: '50%' }} src={flagsPath + flag + '.svg'} alt="flag"></img> {language}
+    <div style={{ display: 'flex' }}>
+      <div style={{
+        width: '30px',
+        height: '30px',
+        position: 'relative',
+        overflow: 'hidden',
+        borderRadius: '50%',
+        marginRight: '10px',
+        border: '1px solid grey'
+      }}>
+        <img style={{
+          display: 'inline',
+          margin: '0 auto',
+          marginLeft: '-25%',
+          marginTop: '-2%',
+          height: '100%',
+          width: 'auto'
+        }} src={flagsPath + flag + '.svg'} alt="flag"></img>
+      </div>{language}
     </div>
   )
 }
